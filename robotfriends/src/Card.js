1c0775331137156ece115c95fa7f250a56853924
iFancy <changfan@usc.edu>
2018-03-22 18:01:02 -0700

    3.22

---
+++ robotfriends/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div>
+			<img alt = 'robot' src = 'https://robohash.org//vbnmkjfgdrwetyrf?set=set4' />
+			<div>
+				<h2> Hane Doe </h2>
+				<p> iane.dou@gmail.com </p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
