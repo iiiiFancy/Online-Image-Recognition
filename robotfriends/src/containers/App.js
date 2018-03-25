41d7c9eafab8fcea44f23aab1192c9d16070feff
iFancy <changfan@usc.edu>
2018-03-25 16:47:48 -0700

    Done

--- robotfriends/src/containers/App.js
+++ robotfriends/src/containers/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import { robots } from './robots';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+//import { robots } from './robots';
+import SearchBox from '../components/SearchBox';
 import'./App.css'
+import Scroll from '../components/Scroll'
 
 // props 静态
 // state 动态
@@ -11,9 +12,15 @@ class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots, // 保存供输出的全部信息
+			robots: [], // 保存供输出的全部信息
 			searchField: '' // 搜索框输入的字符串
 		}
+	}
+
+	componentDidMount() { // make request here
+		fetch('http://jsonplaceholder.typicode.com/users')
+		.then(response => response.json()) // 接受回应
+		.then(users => this.setState({robots:users}));
 	}
 
 	//onSearchChange(event) { 这样写this会指向event 即SearchBox中的<input>
@@ -25,14 +32,16 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 		})
-
-		return (
+		return !this.state.robots.length ? <h1> loading </h1> :
+		(
 			<div className = 'tc'>
 				<h1 className = 'f1'> Cat Friends</h1>
 				<SearchBox searchChange = {this.onSearchChange} /> {/* searchChange触发onSearchChange()修改state */}
-				<CardList robots = {filteredRobots}  />  {/* 返回输入变量中所有元素 */}
+				<Scroll>
+					<CardList robots = {filteredRobots}  />  {/* 以cardlist形式返回输入变量中所有元素 */}
+				</Scroll>
 			</div>
-		);
+		);		
 	}
 }
 
--- robotfriends/src/index.js
+++ robotfriends/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import App from './App';
+import App from './containers/App';
 
 ReactDOM.render(
 	<App /> // cardlist has prop: robots, where robots comes from {robots}
+	// 每当刷新网页 App 组件便挂载在Id为root的document上（见index.html）（用App替代root）
 , document.getElementById('root')
 );
 registerServiceWorker();
