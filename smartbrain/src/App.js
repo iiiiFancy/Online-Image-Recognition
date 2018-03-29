e544b9d65468d78da3dac588d31e0e294a9b3ef5
iFancy <changfan@usc.edu>
2018-03-28 21:30:26 -0700

    components

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import './App.css';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Signin from './components/Signin/Signin';
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 30,
+            density: {
+                enable: true,
+                value_area:800
+            }
+        }
+    }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <Logo />
-        {/* 
-                <ImageLinkForm />
+    render() {
+        return (
+            <div className="App">
+                <Particles className = 'particles'
+                    params = {particlesOptions}
+                />
+                <Navigation/>
+                {/*<Signin />*/}
+                <Logo/>
+                <Rank/>
+                <ImageLinkForm/>
+                {/*
                 <FaceRecongnition /> */}
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ smartbrain/src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Rank display here'}
+            </div>
+
+            <div className='white f1'>
+                {'Rank display here'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
